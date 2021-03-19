bdad3a882fdceda9b803bfa468f5baafcb4a90ec
Nayuta Yanagisawa <nayuta.yanagisawa@hey.com>
2021-03-19 11:13:53 +0900

    Add gdb to dev/bootstrap.sh

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get -y upgrade
 apt-get install -y \
-    silversearcher-ag \
+    silversearcher-ag gdb \
     cmake g++ pkg-config zip unzip libjson-perl # MySQL build and test dependencies
