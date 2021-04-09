158b6c7bc51508bca584f8986b71060610310e11
Nayuta Yanagisawa <nayuta.yanagisawa@hey.com>
2021-04-09 20:30:25 +0900

    Add Docker-related settings to bootstrap.sh

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -3,5 +3,9 @@
 apt-get update
 apt-get -y upgrade
 apt-get install -y \
-    silversearcher-ag gdb \
+    silversearcher-ag gdb docker.io docker-compose \
     cmake g++ pkg-config zip unzip libjson-perl # MySQL build and test dependencies
+
+# Docker
+systemctl enable --now docker
+usermod -aG docker vagrant
